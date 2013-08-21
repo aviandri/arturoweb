345c5d7c6bc33aa9db2df96d40a72a269bfb4f4b
aviandri <aviandri@gmail.com>
2013-08-21 16:07:40 +0700

    initial commit

---
+++ myapp.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+get '/' do
+  	erb :index
+end
+
+
+# post '/sendmail' do
+# 	require 'pony'
+#  	Pony.mail({
+# 	:from => params["contact_email"],
+#     :to => 'aviandri@gmail.com',
+#     :subject => params["contact_email"] + " " +  params["contact_name"],
+#     :body => params["contact_message"],
+#     :via => :smtp,
+#     :via_options => {
+#      :address              => 'smtp.gmail.com',
+#      :port                 => '587',
+#      :enable_starttls_auto => true,
+#      :user_name            => '',
+#      :password             => '',
+#      :authentication       => :plain, 
+#      :domain               => "" 
+#      }
+#     })
+# end
